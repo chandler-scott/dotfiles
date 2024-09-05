828cda994c93373293cd5083a29adbd7efe200f3
Chandler Scott <chandler.scott.dev@gmail.com>
2024-09-05 00:01:50 -0400

    init

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Symlink dotfiles
+ln -sf ~/src/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/src/dotfiles/.bash_aliases ~/.bash_aliases
+ln -sf ~/src/dotfiles/.bash_vars ~/.bash_vars
+ln -sf ~/src/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/src/dotfiles/.vim ~/.vim
+
+# Handle kali
+if [ "$(uname -a | grep -i kali)" ]; then
+    # Kali-specific configurations
+    ln -sf ~/dotfiles/kali/.bash_aliases ~/.bash_aliases
+fi
